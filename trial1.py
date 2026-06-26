3b9a296311a68968fda2cb61da45b7161ff22b5a
Yatharth-Vasu <yatharthvasu2008@gmail.com>
2026-06-26 11:08:09 +0530

    Update trial1.py

--- trial1.py
+++ trial1.py
@@ -1,8 +1,33 @@
 import streamlit as st
-
-option = st.selectbox(
+n1 = st.selectbox(
     "Choose your skin type",
     ["Oily", "Dry", "Combination"]
 )
+st.title("Know Your Cure")
 
-st.write("You selected:", option)
+n2 = st.selectbox("which product do u want" , ["Face wash" , "Toner"])
+if st.button("Identify Cure"):
+
+    if( n1 == "oily" and n2 == "Face wash"  ):
+        print("""Oily & Acne-Prone Skin:
+ *Face Wash* -
+1. The Derma Co 1% Salicylic Acid Gel Face Wash
+2. Minimalist 2% Salicylic Acid Cleanser
+3. Chemist At Play Oil & Acne Control Face Wash
+ *Toner* -
+1. The Derma Co Pore Minimizing Toner
+2. COSRX AHA/BHA Clarifying Treatment Toner
+3. Dot & Key Rice Water Toner
+ *Serum* -
+1. Minimalist Niacinamide 5% Serum
+2. The Ordinary Niacinamide 10% + Zinc 1%
+3. The Derma Co Niacinamide Serum
+ *Moisturizer* -
+1. Pond's Super Light Gel
+2. Minimalist Vitamin B5 10% Moisturizer
+3. Re'equil Oil Free Moisturiser
+ *Sunscreen* -
+1. Deconstruct Gel Sunscreen SPF 55
+2. Fixderma Shadow SPF 50+ Gel
+3. Beauty of Joseon Relief Sun SPF 50+""")
+    
