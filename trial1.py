82361a60e3d18027f753312e68fffe09059ee5e8
Yatharth-Vasu <yatharthvasu2008@gmail.com>
2026-06-26 11:59:45 +0530

    Update trial1.py

--- trial1.py
+++ trial1.py
@@ -16,6 +16,8 @@ if st.button("Identify Cure"):
  *Face Wash* -
  
 1. The Derma Co 1% Salicylic Acid Gel Face Wash – https://thedermaco.com/product/1-salicylic-acid-gel-face-wash-the-dermaco-100ml/
+
 2.Minimalist 2% Salicylic Acid + LHA Cleanser – https://beminimalist.co/products/salicylic-lha-2-cleanser
+
 3.Chemist At Play Oil & Acne Control Face Wash – https://innovist.com/products/oil-acne-control-face-wash """)
 
